edcf62dca283e16df4f6460fd9764f623abaab78
Edoardo <edoardo.vanin@gmail.com>
2017-06-17 22:42:12 +0200

    Aggiunta scaletta di operazioni da svolegere all'avvio del server e definito il numero massimo di utenti

--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "user.h"
+#include "channel.h"
+
+#define MAXUSER 15
+
+/*
+  - inizializzare il socket
+  - ascoltare la porta
+  - accettare le richieste fino ad un max utenti
+  - associare un thread ad ogni utente che si connette
+  */
 
 int main(int argc, char const *argv[]) {
+
   printf("Hello, Word\n");
   return 0;
 }
