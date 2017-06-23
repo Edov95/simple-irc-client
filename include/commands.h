2a2594597b1f583541ec71a0e72d22b4fe412b46
Edoardo <edoardo.vanin@gmail.com>
2017-06-23 21:09:31 +0200

    Definiti i comandi utenti come stringhe

---
+++ include/commands.h
@@ -0,0 +1,29 @@
+/**
+  * @file commands.h
+  * @author Edoardo Vanin
+  * @date 23 Giugno 2017
+  * @brief Definisco i comandi utenti come stringhe i comandi\
+   sono fanno riferimento alla documentazione ufficiale (RFC)
+  */
+#ifndef __COMMANDS_H__
+#define __COMMANDS_H__
+
+extern char *DCC;
+extern char *WHO;
+extern char *NICK;
+extern char *USER;
+extern char *LIST;
+extern char *JOIN;
+extern char *PART;
+extern char *MODE;
+extern char *PING;
+extern char *PONG;
+extern char *QUIT;
+extern char *WHOIS;
+extern char *NOTICE;
+extern char *PRIVMSG;
+extern char *MACDATA;
+extern char *MACHORA;
+extern char *MACTEMPERATURA;
+
+#endif
---
+++ src/commands.c
@@ -0,0 +1,18 @@
+#include "commands.h"
+
+char *NICK           = "NICK";
+char *USER           = "USER";
+char *LIST           = "LIST";
+char *JOIN           = "JOIN";
+char *PART           = "PART";
+char *MODE           = "MODE";
+char *PING           = "PING";
+char *PONG           = "PONG";
+char *WHO            = "WHO";
+char *DCC            = "\001DCC";
+char *WHOIS          = "WHOIS";
+char *QUIT           = "QUIT";
+char *PRIVMSG        = "PRIVMSG";
+char *MACDATA        = "MACDATA";
+char *MACHORA        = "MACHORA";
+char *MACTEMPERATURA = "MACTEMPERATURA";
