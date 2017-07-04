b3c7646d80bf8578dfd9134e385f3ed04b437239
Edoardo <edoardo.vanin@gmail.com>
2017-07-04 11:01:25 +0200

    Rimosso un po' di testo inutile

--- src/text.c
+++ src/text.c
@@ -6,10 +6,6 @@ char *WELCOME_03      = ":irc.labiis.org NOTICE * :*** Pronto!\n";
 char *WELCOME_USER_01 = ":irc.labiis.org 001 ";
 char *WELCOME_USER_02 = " :Benvenuto! il tuo hostname è: ";
 
-char *DUMMY_USER      = "unnamed_user";
-char *DUMMY_CHANNEL   = "*";
-char *DUMMY_HOST      = "unknown_host";
-
 char *SERVER_NAME     = "irc.labiis.org";
 char *SERVER_INFO     = ":Padova, IT, Europa";
 char *DEFAULT_PONG    = ":irc.labiis.org PONG irc.labiis.org :";
@@ -18,10 +14,6 @@ char *ENDOFWHOIS      = " :End of /WHOIS list\n";
 char *ENDOFWHO        = " :End of /WHO list\n";
 char *ENDOFNAMES      = " :End of /NAMES list\n";
 
-char *CHANNELS[]      = {"WhiteHat",
-                         " :No script kiddies allowed.\n",
-                         "1337h4x0rz",
-                         " ::(){ :|: & };:\n"};
 
 char *LIST_END        = " :End of /LIST\n";
 char *LIST_HEADER     = " Channel :Users Name\n";
